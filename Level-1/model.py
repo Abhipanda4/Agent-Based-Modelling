723b8dce57bf20129dddf7d124323b0ef4696298
abhipanda4 <abhipanda03@gmail.com>
2018-08-11 07:11:09 +0530

    initial model done

---
+++ Level-1/model.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+from mesa import Model
+from mesa.space import MultiGrid
+from mesa.time import RandomActivation
+
+from config import *
+from agent import *
+
+class World(Model):
+    def __init__(self, N, width=100, height=100):
+        self.num_agents = N
+        self.grid = MultiGrid(width, height, True)
+        self.schedule = RandomActivation(self)
+        self.running = True
+
+        for i in range(self.num_agents):
+            if np.random.uniform() <= EXPLORER_RATIO:
+                # create a new explorer
+                a = Explorer(i, self)
+            else:
+                a = Exploiter(i, self)
+            self.schedule.add(a)
+            x = np.random.randint(0, self.grid.width)
+            y = np.random.randint(0, self.grid.height)
+            self.grid.place_agent(a, (x, y))
+
+    def step(self):
+        self.schedule.step()
