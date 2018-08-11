9d9f8a75220591ba3271e3fbc33ae2b697111ca8
abhipanda4 <abhipanda03@gmail.com>
2018-08-11 12:09:06 +0530

    added death and communication

--- Level-1/agent.py
+++ Level-1/agent.py
@@ -8,16 +8,38 @@ from config import *
 class SocietyMember(Agent):
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
-        self.memory = []
+        self.memory = None
         self.energy = np.random.normal(MEAN_ENERGY, STDDEV_ENERGY, size=1)
+        self.age = 0
 
     def move(self):
+        ''' Move to a neighboring cell in grid '''
         possible_steps = self.model.grid.get_neighborhood(
                 self.pos,
                 moore=True,
                 include_center=False)
         new_position = random.choice(possible_steps)
         self.model.grid.move_agent(self, new_position)
+
+    def update_memory(self):
+        ''' Add new energy resources to memory '''
+        pass
+
+    def share_memory(self, agent):
+        pass
+
+    def communicate(self, max_range):
+        ''' Communication between 2 agents '''
+        nbrs = self.model.grid.get_neighbors(self.pos, moore=True, radius=max_range)
+        for a in nbrs:
+            if isinstance(a, SocietyMember):
+                if np.random.uniform() <= COMMUNICATION_PROB:
+                    self.share_memory(a)
+
+    def die(self):
+        ''' Removes the agent from gridworld and scheduler '''
+        self.model.schedule.remove(self)
+        self.model.grid.remove_agent(self)
 
     def step(self):
         raise NotImplementedError
@@ -32,7 +54,10 @@ class Explorer(SocietyMember):
 
     def step(self):
         self.move()
+        self.age += 1
         self.energy -= self.stamina
+        if self.energy <= 0:
+            self.die()
 
 class Exploiter(SocietyMember):
     def __init__(self, unique_id, model):
@@ -43,5 +68,11 @@ class Exploiter(SocietyMember):
         self.sense_range = EXPLOITER_SENSE_RANGE
 
     def step(self):
-        self.move()
-        self.energy -= self.stamina
+        self.age += 1
+        if self.memory is not None:
+            # try to explore with some small prob
+            self.move()
+            self.energy -= self.stamina
+
+        if self.energy <= 0:
+            self.die()
