bab330378014390e045a482be44ae068ffd9bd2c
abhipanda4 <abhipanda03@gmail.com>
2018-08-12 15:10:49 +0530

    energy reserves added as agent to env

--- Level-1/model.py
+++ Level-1/model.py
@@ -13,21 +13,38 @@ class World(Model):
         self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         self.running = True
+        self.population_center_x = np.random.randint(POP_MARGIN, self.grid.width - POP_MARGIN)
+        self.population_center_y = np.random.randint(POP_MARGIN, self.grid.height - POP_MARGIN)
+        self.num_energy_resources = RESERVE_SIZE
 
+        # add social agents to the world
         for i in range(self.num_agents):
             if np.random.uniform() <= EXPLORER_RATIO:
                 # create a new explorer
-                a = Explorer(i, self)
+                a = Explorer("explorer_%d" %(i), self)
             else:
                 # create a new exploiter
-                a = Exploiter(i, self)
+                a = Exploiter("exploiter_%d" %(i), self)
+
+            # keep society members confined at beginning
+            x = np.random.randint(self.population_center_x - POP_SPREAD, self.population_center_x + POP_SPREAD)
+            y = np.random.randint(self.population_center_y - POP_SPREAD, self.population_center_y + POP_SPREAD)
+            self.grid.place_agent(a, (x, y))
 
             # add agent to scheduler
             self.schedule.add(a)
 
+        # add energy reserves to the world
+        for i in range(self.num_energy_resources):
+            a = EnergyResource("energy_reserve_%d" %(i), self)
+
+            # decide location of energy reserve
             x = np.random.randint(0, self.grid.width)
             y = np.random.randint(0, self.grid.height)
             self.grid.place_agent(a, (x, y))
+
+            self.schedule.add(a)
+
 
     def step(self):
         self.schedule.step()
