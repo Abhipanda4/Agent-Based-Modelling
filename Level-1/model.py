d24eba6ac632e70094ebe24f41640e850d98b0ea
abhipanda4 <abhipanda03@gmail.com>
2018-09-03 18:42:15 +0530

    added provision for spawn of new energy

--- Level-1/model.py
+++ Level-1/model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from mesa import Model
 from mesa.space import MultiGrid
@@ -16,16 +17,22 @@ class World(Model):
         self.population_center_x = np.random.randint(POP_MARGIN, self.grid.width - POP_MARGIN)
         self.population_center_y = np.random.randint(POP_MARGIN, self.grid.height - POP_MARGIN)
         self.num_energy_resources = RESERVE_SIZE
-        self.dead_people = 0
+        self.ages = []
+        self.expected_ages = []
+        self.num_explorers = 0
+        self.num_exploiters = 0
+        self.bases = self.init_base()
 
         # add social agents to the world
-        for i in range(self.num_agents):
+        for i in range(1, self.num_agents + 1):
             if np.random.uniform() <= EXPLORER_RATIO:
                 # create a new explorer
                 a = Explorer("explorer_%d" %(i), self)
+                self.num_explorers += 1
             else:
                 # create a new exploiter
                 a = Exploiter("exploiter_%d" %(i), self)
+                self.num_exploiters += 1
 
             # keep society members confined at beginning
             x = np.random.randint(self.population_center_x - POP_SPREAD, self.population_center_x + POP_SPREAD)
@@ -34,6 +41,7 @@ class World(Model):
 
             # add agent to scheduler
             self.schedule.add(a)
+            self.expected_ages.append(a.energy / a.living_cost)
 
         # add energy reserves to the world
         for i in range(self.num_energy_resources):
@@ -46,5 +54,23 @@ class World(Model):
 
             self.schedule.add(a)
 
+    def init_base(self):
+        pos = (self.population_center_x, self.population_center_y)
+        return self.grid.get_neighborhood(pos, True, radius=POP_SPREAD)
+
     def step(self):
         self.schedule.step()
+        if self.schedule.steps % NEW_ENERGY_STEPS == 0:
+            if np.random.uniform() < NEW_ENERGY_PROB:
+                self.num_energy_resources += 1
+                a = EnergyResource("energy_reserve_%d" %(self.num_energy_resources), self)
+
+                # decide location of energy reserve
+                x = np.random.randint(0, self.grid.width)
+                y = np.random.randint(0, self.grid.height)
+                self.grid.place_agent(a, (x, y))
+
+                self.schedule.add(a)
+
+        # log info about the world
+        print(self.num_explorers, self.num_exploiters)
